2c5f51219d48eb3a30c95b79c1b87086c9c75483
Welinton Martins <57647104+welinton-martins@users.noreply.github.com>
2021-08-19 21:28:25 +0000

    triangle area

---
+++ Introducao/Ex3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int l1, l2, l3, area;
+	cout << "Informe os lados do triangulo: ";
+	cin >> l1 >> l2 >> l3;
+	area = l1 * l2 * l3;
+	cout << "Area do triangulo" << area;
+}
