7d921ffbbfd60c40c8e957272685d30c375b8350
welinton-martins <welintonmartins4@outlook.com>
2021-08-19 15:21:46 -0300

    declaring 2 dimensional array

--- Matriz/Ex1.cpp
+++ Matriz/Ex1.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
-    int mat[4][4], som_diag = 0;
+    int mat[4][4] = {
+	    {1,2,3,4},
+	    {1,1,3,4},
+	    {1,2,1,4},
+	    {1,2,3,1}
+    }, som_diag = 0;
 
     for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
