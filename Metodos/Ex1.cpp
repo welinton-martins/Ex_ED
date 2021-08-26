e2afe62d5fefdd079fd21e0af183997940130307
welinton-martins <welintonmartins4@outlook.com>
2021-08-26 18:00:09 +0000

    temperature

--- Metodos/Ex1.cpp
+++ Metodos/Ex1.cpp
@@ -1,7 +1,31 @@
+/*Desenvolva um programa que tenha dois métodos fahrenheit e celsius,
+onde você entra com um valor em fahrenheit e transforma em celsius e vice e versa.
+Fórmula Celsius: (fahrenheit – 32) * 5/9;
+Fahrenheit = (Celsius * 9/5) + 32;*/
+
 #include <iostream>
 
 using namespace std;
 
+float celsius(int celsius) {
+    return (celsius  * 9/5) + 32;
+}
+
+float fahrenheit(int fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
 int main() {
+    int opcao = 0, temperatura = 0;
+    cout << "Conversor de Celsius/Fahrenheit" << endl;
+    cout << "Opcao:\nCelsius=1\nFahrenheit=2\n";
+    cin >> opcao;
+    cout << "Informe a temperatura: ";
+    cin >> temperatura;
+    if (opcao == 1) {
+        cout << "Temperatura de Celsius para Fahrenheit: " << celsius(temperatura) << endl;
+    } else if (opcao == 2) {
+        cout << "Temperatura de Fahrenheit para Celsius: " << fahrenheit(temperatura) << endl;
+    }
 	return 0;
 }
